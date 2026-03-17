cb32e29bcc0d8248b637769bcab55b8aa6aaefc7
JayTongue <jaytongue@gmail.com>
2026-03-17 17:01:52 -0500

    working on discussion

--- render_webpage.sh
+++ render_webpage.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-
-SCENES=$(grep -v '^\s*#' slides.py | grep -oP '(?<=^class )\w+(?=\(Slide\))' | tr '\n' ' ')
+SCENES=$(grep -v '^\s*#' slides.py | grep -oP '(?<=^class )\w+(?=\(.*Slide)' | tr '\n' ' ')
 echo "Rendering scenes: $SCENES"
-
-# manim -qm --disable_caching slides.py $SCENES
-manim -qm  slides.py $SCENES
-# manim -qm --flush_cache slides.py $SCENES
+manim -qm slides.py $SCENES
 manim-slides convert $SCENES slides.html
+
+# manim -qh slides.py $SCENES
+# manim-slides convert --one-file $SCENES one_shot_slides.html
